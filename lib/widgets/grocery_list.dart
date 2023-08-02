a1b77ab1215f62dcbc0f91cbf9ecfc0fe71c96f3
Bolutife-6978 <ogunfowokansherrifdeen@gmail.com>
2023-08-02 20:57:37 +0100

    My solution

--- lib/widgets/grocery_list.dart
+++ lib/widgets/grocery_list.dart
@@ -1,14 +1,23 @@
 import 'package:flutter/material.dart';
 import 'package:shopping_list_app/data/dummy_items.dart';
+import 'package:shopping_list_app/widgets/new_item.dart';
 
 class GroceryList extends StatelessWidget {
-  const GroceryList();
+  const GroceryList({super.key});
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
         title: const Text('Your Groceries'),
+        actions: [
+          IconButton(
+              onPressed: () {
+                Navigator.push(
+                    context, MaterialPageRoute(builder: (ctx) => const NewItem()));
+              },
+              icon: const Icon(Icons.add))
+        ],
       ),
       body: ListView.builder(
           itemCount: groceryItems.length,
